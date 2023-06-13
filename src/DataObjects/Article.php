e49b62edd80c55475daaad4487e6056431b2e52a
jeFFF @ Tisseur de toile <jft@tisseur-de-toile.fr>
2023-06-13 12:04:49 +0200

    Move file

---
+++ src/DataObjects/Article.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Jefffairson\WPArticles;
+
+use Carbon\CarbonImmutable;
+use Spatie\LaravelData\Data;
+use Spatie\LaravelData\Attributes\MapInputName;
+use Spatie\LaravelData\Attributes\WithTransformer;
+use Spatie\LaravelData\Transformers\DateTimeInterfaceTransformer;
+
+class Article extends Data
+{
+    public function __construct(
+        #[MapInputName('ID')]
+        public int $id,
+        #[MapInputName('post_title')]
+        public string $title,
+        #[
+            MapInputName('post_date'),
+            WithTransformer(DateTimeInterfaceTransformer::class)
+        ]
+        public CarbonImmutable $date,
+        #[MapInputName('post_excerpt')]
+        public string $excerpt,
+        #[MapInputName('post_content')]
+        public string $content,
+        #[MapInputName('post_status')]
+        public string $status,
+        #[MapInputName('post_type')]
+        public string $type,
+    ) {}
+}
