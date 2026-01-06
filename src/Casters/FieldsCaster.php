c82771a4bbc796afdf67ffefcc7976cab94d94d9
jeFFF @ Tisseur de toile <jft@tisseur-de-toile.fr>
2026-01-06 10:25:00 +0100

    Refactor ArticleTermsCaster and FieldsCaster to use properties array instead of context array in cast method

--- src/Casters/FieldsCaster.php
+++ src/Casters/FieldsCaster.php
@@ -4,20 +4,20 @@ namespace Jefffairson\WPArticles\Casters;
 
 use Spatie\LaravelData\Casts\Cast;
 use Jefffairson\WPArticles\Tools\AcfFields;
-use Jefffairson\WPArticles\DataObjects\Term;
 use Spatie\LaravelData\Support\DataProperty;
+use Spatie\LaravelData\Support\Creation\CreationContext;
 
 class FieldsCaster implements Cast
 {
-    public function cast(DataProperty $property, mixed $value, array $context): mixed
+    public function cast(DataProperty $property, mixed $value, array $properties, CreationContext $context): mixed
     {
         $key = '';
-        if (isset($context['term_id'])) {
-            $key = $context['taxonomy'] . '_' . $context['term_id'];
+        if (isset($properties['term_id'])) {
+            $key = $properties['taxonomy'] . '_' . $properties['term_id'];
         }
 
-        if (isset($context['ID'])) {
-            $key = $context['ID'];
+        if (isset($properties['ID'])) {
+            $key = $properties['ID'];
         }
 
         $fields = collect(get_fields($key))
